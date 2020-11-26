5a28638e8ee698d49957e57434f15fa9b788353a
Manue <manue.ggt@gmail.com>
2020-11-26 09:05:31 +0100

    exo00 ok

--- ex00/Single.php
+++ ex00/Single.php
@@ -3,4 +3,18 @@
 final class Single
 {
 //  Your code here
+    private static $instance;
+
+    public function __construct(){}
+
+    public static function getInstance(): Single
+    {
+        if (!self::$instance){
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+
+
 }

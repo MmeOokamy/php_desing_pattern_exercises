ccfc97fdb5fdea038097212e0b5e5e708fa23181
Manue <manue.ggt@gmail.com>
2020-11-26 10:22:04 +0100

    exo 3 - 00

--- ex03/ex00/ComputerFactory.php
+++ ex03/ex00/ComputerFactory.php
@@ -7,6 +7,13 @@ class ComputerFactory
     // Your code here
     public static function makeComputer($brand): Computer
     {
-        return new ComputerDell($brand);
+        switch ($brand){
+            case 'Dell':
+            $created = new ComputerDell($brand);
+            break;
+            default:
+                throw new Exception("Je ne connais pas cette marque");
+        }
+        return $created;
     }
 }

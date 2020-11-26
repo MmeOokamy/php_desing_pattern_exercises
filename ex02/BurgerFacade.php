ea6af74c60d307e7efc0a6806c4b05d79361260e
Manue <manue.ggt@gmail.com>
2020-11-26 10:03:59 +0100

    burger is done, but i can't eat  :cry:

--- ex02/BurgerFacade.php
+++ ex02/BurgerFacade.php
@@ -2,4 +2,32 @@
 class BurgerFacade
 {
 //   Your code here
+    protected $burger;
+
+    public function __construct(Burger $makeBurger)
+    {
+        $this->burger = $makeBurger;
+    }
+
+    /**
+     * @return Burger
+     */
+    public function getBurger(): Burger
+    {
+        $this->burger->getBread();
+        $this->burger->cookSteak();
+        $this->burger->putTheVegetables();
+        $this->burger->serve();
+        return $this->burger;
+    }
+
+
+    public function eatBurger(): Burger
+    {
+        $this->burger->unpack();
+        $this->burger->eat();
+        $this->burger->clearTheTable();
+        return $this->burger;
+    }
+
 }
